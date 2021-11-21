a9b35312139906e1ed5445db0632ae9192ed0286
kk00563160 <90852528+kk00563160@users.noreply.github.com>
2021-11-21 16:11:54 +0530

    Enable cors

--- src/stock-user-bootstrapper.ts
+++ src/stock-user-bootstrapper.ts
@@ -4,6 +4,7 @@ import { ConfigService } from "./infrastructure/configuration/config.service";
 
 async function bootstrap() {
     const app = await NestFactory.create(UserModule)
+    app.enableCors();
     var port = ConfigService.create().getPort();
     console.log(port);
     await app.listen(port);
